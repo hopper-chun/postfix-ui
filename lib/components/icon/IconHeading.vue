5c10324c01b8685ad076754cd3395478484efc84
khs <envient92@gmail.com>
2024-08-23 17:04:30 +0900

    아이콘 추가

---
+++ lib/components/icon/IconHeading.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M7 4.49316V11.9932V19.4952M7 11.9942H17.5M17.5 11.9942V4.49416M17.5 11.9942V19.4952"
+      stroke="currentColor"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
