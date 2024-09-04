bba21110e178cf85d0c00f49f9427f20f232026a
khs <envient92@gmail.com>
2024-09-04 12:15:38 +0900

    아이콘 옮기는중

--- lib/components/icon/IconList.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <svg viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect x="3" y="3" width="12" height="12" rx="1" fill="currentColor" />
-    <rect opacity="0.5" x="4.71484" y="4.71436" width="1.71429" height="8.57143" rx="0.857143" fill="white" />
-    <rect x="8.14258" y="6.42871" width="1.71429" height="6.85714" rx="0.857143" fill="white" fill-opacity="0.5" />
-    <rect x="11.5723" y="9" width="1.71429" height="4.28571" rx="0.857143" fill="white" />
-  </svg>
-</template>
